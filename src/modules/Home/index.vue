f12529cd0880953c706e631dcd3d6e7c5ae389a1
lijinze <1528562348@qq.com>
2019-11-30 22:12:41 +0800

    初始化

---
+++ src/modules/Home/index.vue
@@ -0,0 +1,32 @@
+<!-- Home -->
+<template>
+  <div class="home">
+    <div class="title">title</div>
+    <div class="content">content</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  created () {}
+}
+</script>
+<style lang='scss' scoped>
+.home {
+  .title {
+    color: $--founder-default-color;
+    font-size: $--founder-title-size;
+  }
+  .content {
+    color: $--founder--color-900;
+    font-size: $--founder-content-size;
+  }
+}
+</style>
